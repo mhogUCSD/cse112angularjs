dace8bd5191263024e1cabd046287d7b45d3ef86
mhogetvedt <mhogetve@ucsd.edu>
2015-02-09 17:44:57 -0800

    Front-end Profile HW; templates

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,11 @@
 'use strict';
 
 /* Controllers */
+
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('BGColorSel', function($scope) {
+  $scope.color = "#ffffff";
+  $scope.secondColor = "#ffffff";
+  $scope.thirdColor = "#ffffff";
+});
